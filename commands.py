459ab2fbea1887214b65073b85a7bf15f7275012
K3nny13 <kenneth_cogzell@hotmail.com>
2020-07-18 17:28:58 +0200

    Added commands.py

---
+++ commands.py
@@ -0,0 +1,28 @@
+import database_manager
+
+db = database_manager.DatabaseManager('bookmarks.db')
+
+class CreateBookmarksTableCommand:
+    def execute(self):
+        db.create_table('bookmarks', {
+            'id': 'integer primary key autoincrement',
+            'title': 'text not null',
+            'url': 'text not null',
+            'notes': 'text',
+            'date_added': 'text not null',
+        })
+
+from datetime import datetime
+
+class AddBookmarkCommand:
+    def execute(self, data):
+        data['date_added'] = datetime.utcnow().isoformat()
+        db.add('bookmarks', data)
+        return 'Bookmark added!'
+
+class ListBookmarksCommand:
+    def __init__(self, order_by='date_added'):
+        self.order_by = order_by
+
+    def execute(self):
+        return db.select('bookmarks', order_by=self.order_by).fetchall()
--- database_manager.py
+++ database_manager.py
@@ -14,11 +14,54 @@ def create_table(self, table_name, columns):
                   '''
                   )
 
-    def _execute(self, statement):
+    def _execute(self, statement, values):
         with self.connection:
             cursor = self.connection.cursor()
             cursor.execute(statement, values or [])
             return cursor
+
+    def add(self, table_name, data):
+        placeholders = ', '.join('?' * len(data))
+        column_names = ', '.join(data.keys())
+        column_values = tuple(data.values())
+
+        self._execute(
+            f'''
+            INSERT INTO {table_name}
+            ({column_names})
+            VALUES ({placeholders});
+            ''',
+            column_values,
+            )
+
+    def delete(self, table_name, criteria):
+        placeholders = [f'{column} = ?' for column in criteria.keys()]
+        delete_criteria = ' AND '.join(placeholders)
+        self._execute(
+            f'''
+            DELETE FROM {table_name}
+            WHERE {delete_criteria};
+            ''',
+            tuple(criteria.values()),
+        )
+
+    def select(self, table_name, criteria=None, order_by=None):
+        criteria = criteria or {}
+
+        query = f'SELECT * FROM {table_name}'
+
+        if criteria:
+            placeholders = [f'{column} = ?' for column in criteria.keys()]
+            select_criteria = ' AND '.join(placeholders)
+            query += f' WHERE {select_criteria}'
+
+        if order_by:
+            query += f' ORDER BY {order_by}'
+
+        return self._execute(
+            query,
+            tuple(criteria.values()),
+        )
 
     def __del__(self):
         self.connection.close()
